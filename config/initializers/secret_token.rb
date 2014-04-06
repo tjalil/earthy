5c26c0fc21a59de07c0b0a1ad364dd4622544960
Taha Jalil <taha.jalil@gmail.com>
2014-04-06 13:52:15 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Earthy::Application.config.secret_key_base = 'a8b5f2faeca51c28a4f9c90234221888726c309de06fc89c5778e44f9e3bb974b8a5033fae909c8f3f4804cdb01ce6e4fc31e8115f89f50f6537f4ebff78953b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Earthy::Application.config.session_store :cookie_store, key: '_earthy_session'
