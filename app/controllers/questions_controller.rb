8dba9e5c83dd199da80f17fa12804b1940276733
Steve Long <steven989@gmail.com>
2014-04-12 16:21:43 -0400

    Started writing the code to keep track of rounds

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,18 +1,41 @@
 class QuestionsController < ApplicationController
+
+  @counter = []
+
   def index
 
   end
 
   def ask
     
-    @question = Question.random_question
+
+    counter_passed = params[:counter]
+
+    counter_passed_array = counter_passed.split("|")
+
+    if counter_passed_array.length >= 6
+
+      redirect_to end_path
+
+    else
+
+      @question = Question.random_question(counter_passed_array)
+
+      counter_passed_array << @question.id
+
+      @counter_to_pass = counter_passed_array.join("|")
+
+    end
 
   end
 
   def validate
 
     choice = params[:answer]
+
     question_id = params[:question_id]
+
+    @counter_to_pass = params[:counter]
 
     @answer = Question.answer_question(question_id,choice)
 
