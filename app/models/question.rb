95f14120f02c25fb5352053b25ff446808621f8e
Steve Long <steven989@gmail.com>
2014-04-12 14:20:18 -0400

    added two methods to pull random question and evaluate answer - no rounds

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,8 +3,7 @@ class Question < ActiveRecord::Base
 
  def self.random_question
 
-        if  @questions_administered.length < 6          
-
+                                                                                       # if  @questions_administered.length < 6         
             max_id = Question.maximum(:id)
             min_id = Question.minimum(:id)
 
@@ -16,18 +15,18 @@ class Question < ActiveRecord::Base
 
             end
 
-            @questions_administered << random_q_id
+                                                                         # @questions_administered << random_q_id
 
             @current_question = Question.find(random_q_id)  #captured in an instance variable so that it can be used by the evaluate_question method
 
             @current_question
 
-        else
+                                                                            # else
 
-            # self.initialize
-            false
+                                                                         #     # self.initialize
+                                                                         #     false
 
-        end
+                                                                             # end
         
 
     end 
@@ -37,5 +36,15 @@ class Question < ActiveRecord::Base
 # Need to code 2 methods in here:
 #1. pull_question_array -- returns array with 6 ids
 #2. fetch_question -- takes 1st element of ids and return question object assoc w/ it
+
+    def self.answer_question(q_id,choice)
+
+        question = Question.find(q_id)
+
+        choice == question.correct_choice.to_i
+
+
+    end 
+
 
 end
