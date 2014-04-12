9b761901fbd6728778b6a2cdcb1484781d1778f1
domicode <domi@domidesign.ch>
2014-04-12 11:33:59 -0400

    added seeds

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20140412142905) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "questions", force: true do |t|
     t.string   "location_title"
