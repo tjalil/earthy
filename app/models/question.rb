f29356adc4b06233378889c404519fb69466d17f
Ian Cantor <icantor@fandm.edu>
2014-04-12 12:57:05 -0400

    made question class notes and added temporary ask.erb file to test routes

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,7 @@
 class Question < ActiveRecord::Base
+
+# Need to code 2 methods in here:
+#1. pull_question_array -- returns array with 6 ids
+#2. fetch_question -- takes 1st element of ids and return question object assoc w/ it
+
 end
